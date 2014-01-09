70c00b99e62c25aab4dd34fd79747358a538708e
2011531027 <haxxty_pyonl5@ezweb.ne.jp>
2014-01-09 16:06:00 +0900

    電卓

--- dentaku neeeeeew/ViewController.h
+++ dentaku neeeeeew/ViewController.h
@@ -8,6 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *Label;
+
+    BOOL startInput;
+    int currentValue;
+    int operation;
+    }
+
+-(IBAction)pushedNumberOneButton:(id)sender;
+-(IBAction)pushedNumberTwoButton:(id)sender;
+-(IBAction)pushedNumberThreeButton:(id)sender;
+-(IBAction)pushedNumberFourButton:(id)sender;
+-(IBAction)pushedNumberFiveButton:(id)sender;
+-(IBAction)pushedNumberSixButton:(id)sender;
+-(IBAction)pushedNumberSevenButton:(id)sender;
+-(IBAction)pushedNumberEightButton:(id)sender;
+-(IBAction)pushedNumberNineButton:(id)sender;
+-(IBAction)pushedNumberZeroButton:(id)sender;
+
+-(IBAction)pushedDivisionButton:(id)sender;
+-(IBAction)pushedMultiplicationButton:(id)sender;
+-(IBAction)pushedSubtractionButton:(id)sender;
+-(IBAction)pushedAdditionButton:(id)sender;
+
+
+-(IBAction)pushedEqualButton:(id)sender;
+
+-(IBAction)pushedAllClearButton:(id)sender;
+-(IBAction)pushedClearButton:(id)sender;
+
+- (void)numberButton:(int)number;
 
 @end
--- dentaku neeeeeew/ViewController.m
+++ dentaku neeeeeew/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    double x;
+    double y;
+    double z;
+}
 
 @end
 
@@ -18,6 +22,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    startInput = YES;
+    currentValue = 0;
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +33,99 @@
     // Dispose of any resources that can be recreated.
 }
 
+    - (IBAction)pushedNumberOneButton:(id)sender {
+        [self numberButton:1];
+           }
+
+    - (void)numberButton:(int)number{
+    x = x*10 + number;
+    Label.text = [NSString stringWithFormat:@"%.f",x];
+    
+}
+
+
+    - (IBAction)pushedNumberTwoButton:(id)sender {
+        [self numberButton:2];
+    }
+    - (IBAction)pushedNumberThreeButton:(id)sender {
+        [self numberButton:3];
+    }
+    - (IBAction)pushedNumberFourButton:(id)sender {
+        [self numberButton:4];
+    }
+    - (IBAction)pushedNumberFiveButton:(id)sender {
+        [self numberButton:5];
+    }
+    - (IBAction)pushedNumberSixButton:(id)sender {
+        [self numberButton:6];
+    }
+    - (IBAction)pushedNumberSevenButton:(id)sender {
+        [self numberButton:7];
+    }   
+    - (IBAction)pushedNumberEightButton:(id)sender {
+        [self numberButton:8];
+    }
+    - (IBAction)pushedNumberNineButton:(id)sender {
+        [self numberButton:9];
+    }
+    - (IBAction)pushedNumberZeroButton:(id)sender {
+        [self numberButton:0];
+    }
+
+
+
+    - (IBAction)pushedDivisionButton:(id)sender {
+        y = x;
+        x = 0;
+        z = 1;
+    }
+    - (IBAction)pushedMultiplicationButton:(id)sender {
+        y = x;
+        x = 0;
+        z = 2;
+    }
+    - (IBAction)pushedSubtractionButton:(id)sender {
+        y = x;
+        x = 0;
+        z = 3;
+    }
+    - (IBAction)pushedAdditionButton:(id)sender {
+        y = x;
+        x = 0;
+        z = 4;
+    }
+
+
+
+    - (IBAction)pushedEqualButton:(id)sender {
+        if(z == 1){
+            x = y / x;
+            Label.text = [NSString stringWithFormat:@"%.2f",x];
+        }
+        
+        if(z == 2){
+            x = y * x;
+            Label.text = [NSString stringWithFormat:@"%.f",x];
+        }
+
+        if(z == 3){
+            x = y - x;
+            Label.text = [NSString stringWithFormat:@"%.f",x];
+        }
+
+        if(z == 4){
+            x = y + x;
+            Label.text = [NSString stringWithFormat:@"%.f",x];
+        }
+
+        
+    }
+    - (IBAction)pushedAllClearButton:(id)sender {
+        x = 0;
+        Label.text = [NSString stringWithFormat:@"%.f",x];
+    }
+    - (IBAction)pushedClearButton:(id)sender {
+        
+        Label.text = [NSString stringWithFormat:@"%.f",x];
+    }
 @end
